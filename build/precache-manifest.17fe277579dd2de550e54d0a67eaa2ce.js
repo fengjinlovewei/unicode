dc722c1d13d66c6a66d0c286eae8d32990c841de
fengjin02 <fengjin02@58.com>
2023-02-22 17:22:15 +0800

    style: 修改路径

---
+++ build/precache-manifest.17fe277579dd2de550e54d0a67eaa2ce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd4b2dc243b222bfa0d9c2ca86743f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ccf17f6f7b8856332241",
+    "url": "./static/css/2.24f7321e.chunk.css"
+  },
+  {
+    "revision": "27b4bd2b532702d2e964",
+    "url": "./static/css/main.8198308f.chunk.css"
+  },
+  {
+    "revision": "ccf17f6f7b8856332241",
+    "url": "./static/js/2.ec9c38f2.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "./static/js/2.ec9c38f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b4bd2b532702d2e964",
+    "url": "./static/js/main.b3726d27.chunk.js"
+  },
+  {
+    "revision": "f53bfb761908ee943efd",
+    "url": "./static/js/runtime-main.d1d9093f.js"
+  },
+  {
+    "revision": "1044ad69d2db79a07045f085ae7b9668",
+    "url": "./static/media/ascii.1044ad69.jpeg"
+  },
+  {
+    "revision": "51435d20e1f4bf77d4a74aa14476ff3b",
+    "url": "./static/media/iconfont.51435d20.woff"
+  },
+  {
+    "revision": "ac6a3ab0349338b4b4a1f841b1747990",
+    "url": "./static/media/iconfont.ac6a3ab0.woff"
+  },
+  {
+    "revision": "e4bc354909592203c1e7e3a61efa4b5d",
+    "url": "./static/media/iconfont.e4bc3549.ttf"
+  },
+  {
+    "revision": "eed5f8de7d124bbf509baa35d106b470",
+    "url": "./static/media/iconfont.eed5f8de.ttf"
+  },
+  {
+    "revision": "63333d533c8f3878c6e0add575924eb5",
+    "url": "./static/media/iso_8859_1.63333d53.jpeg"
+  }
+]);
